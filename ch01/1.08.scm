c7b5c1429af5bc7a5478a594f0d3ec47a8bcdbe3
Fabienne Ducroquet <fabiduc@gmail.com>
2016-02-08 20:23:50 +0100

    Exercise 1.8

---
+++ ch01/1.08.scm
@@ -0,0 +1,21 @@
+(define (cube x)
+  (* x x x))
+
+(define (cube-iter guess x)
+  (define new-guess (improve guess x))
+  (if (good-enough? guess new-guess)
+      guess
+      (cube-iter new-guess
+                 x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+	(* 2 guess))
+     3))
+
+(define (cuberoot x)
+  ; Call cube-iter only with positive values because otherwise
+  ; (improve guess x) can return 0, e.g. with (improve-guess 1.0 -2)
+  (if (>= x 0)
+    (cube-iter 1.0 x)
+    (- (cube-iter 1.0 (- x)))))
