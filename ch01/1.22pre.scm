ff47d2d550335f1d189521e5f25542b5e767257e
Fabienne Ducroquet <fabiduc@gmail.com>
2016-02-26 16:27:17 +0100

    Code from exercise 1.22

---
+++ ch01/1.22pre.scm
@@ -0,0 +1,15 @@
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  (display "\n"))
+
+(define (runtime) (cpu-time))
