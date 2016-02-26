5a3dda9e2750b5debb22dde647ff81a7dc6f3ee4
Fabienne Ducroquet <fabiduc@gmail.com>
2016-02-26 17:46:46 +0100

    Exercise 1.26

---
+++ ch01/1.26pre.scm
@@ -0,0 +1,9 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod base (/ exp 2) m)
+                       (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
