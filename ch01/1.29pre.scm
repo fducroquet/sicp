74f272633bbd0195543eb5cf1508e39ef5b2e7b0
Fabienne Ducroquet <fabiduc@gmail.com>
2016-02-27 16:12:51 +0100

    Code from section 1.3.1

---
+++ ch01/1.29pre.scm
@@ -0,0 +1,45 @@
+(define (sum-integers a b)
+  (if (> a b)
+    0
+    (+ a (sum-integers (+ a 1) b))))
+
+(define (cube x)
+  (* x x x))
+
+(define (sum-cubes a b)
+  (if (> a b)
+    0
+    (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+    0
+    (+ (/ 1.0 (* a (+ a 2)))
+       (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
