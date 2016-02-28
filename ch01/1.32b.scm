5d392167df782b23dbc38cebb18256de90fc6270
Fabienne Ducroquet <fabiduc@gmail.com>
2016-02-28 16:06:41 +0100

    Exercise 1.32

---
+++ ch01/1.32b.scm
@@ -0,0 +1,7 @@
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a)
+            (combiner result (term a)))))
+  (iter a null-value))
