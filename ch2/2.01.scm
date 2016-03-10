c0d903f797fa2df0f108dc7f067848a704a37575
Fabienne Ducroquet <ducroquetf@gmail.com>
2016-03-10 10:04:29 +0100

    Exercise 2.1

---
+++ ch2/2.01.scm
@@ -0,0 +1,8 @@
+(define (make-rat n d)
+  (let ((g (gcd n d))
+	(sign (* n d)))
+    (if (> sign 0)
+      (cons (/ (abs n) g)
+	    (/ (abs d) g))
+      (cons (/ (- (abs n)) g)
+	    (/ (abs d) g)))))
