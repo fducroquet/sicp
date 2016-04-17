66fafe5aae0ed7c9ee2633e65d49f1bc12701c7b
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-17 20:46:10 +0200

    Exercise 2.22

---
+++ ch2/2.22pre.scm
@@ -0,0 +1,17 @@
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (cons (square (car things))
+                  answer))))
+  (iter items '()))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (cons answer
+                  (square (car things))))))
+  (iter items '()))
