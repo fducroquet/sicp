b631b8b9e38ad107b74561f81230296b286e78f6
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-17 20:15:13 +0200

    Exercise 2.19

---
+++ ch2/2.19.scm
@@ -0,0 +1,8 @@
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
