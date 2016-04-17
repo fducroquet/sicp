060b2e70c835e4a41640ade672ad5c2c043e449f
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-17 20:45:59 +0200

    Exercise 2.21

---
+++ ch2/2.21.scm
@@ -0,0 +1,8 @@
+(define (square-list items)
+  (if (null? items)
+    '()
+    (cons (square (car items))
+          (square-list (cdr items)))))
+
+(define (square-list items)
+  (map square items))
---
+++ ch2/2.21pre.scm
@@ -0,0 +1,15 @@
+(define (scale-list items factor)
+  (if (null? items)
+    '()
+    (cons (* (car items) factor)
+          (scale-list (cdr items) factor))))
+
+(define (map proc items)
+  (if (null? items)
+    '()
+    (cons (proc (car items))
+          (map proc (cdr items)))))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
