731b0ce1df0a921af187832ab59ef32a83ecdf9e
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-19 19:49:42 +0200

    Exercise 2.33

---
+++ ch2/2.33.scm
@@ -0,0 +1,8 @@
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
