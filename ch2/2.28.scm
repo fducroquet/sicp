ae60448a143597985b4a5fbe17478bc985932ad7
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-19 17:58:41 +0200

    Exercise 2.28

---
+++ ch2/2.28.scm
@@ -0,0 +1,8 @@
+(define (fringe tree)
+  (cond ((null? tree)
+         '())
+        ((not (pair? tree))
+         (list tree))
+        (else
+          (append (fringe (car tree))
+                  (fringe (cdr tree))))))
