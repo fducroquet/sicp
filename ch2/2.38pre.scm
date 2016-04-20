86892360db0185289740c7b8bbf4a518e1cd664d
Fabienne Ducroquet <fabiduc@gmail.com>
2016-04-20 18:51:55 +0200

    Exercise 2.38

---
+++ ch2/2.38pre.scm
@@ -0,0 +1,7 @@
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest))
+            (cdr rest))))
+  (iter initial sequence))
