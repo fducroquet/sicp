5a586f650c48fa1fa04146a446ce9a93c8448d63
Fabienne Ducroquet <fabiduc@gmail.com>
2016-05-09 21:19:24 +0200

    picture-language.scm file
    File loading all the answers to the exercises in an order that does not cause
    errors because of undefined functions in racket.

---
+++ ch2/picture-language.scm
@@ -0,0 +1,39 @@
+(load "2.44pre2.scm")
+
+(load "2.46pre.scm")
+(load "2.46a.scm")
+(load "2.46b.scm")
+
+(load "2.48.scm")
+(load "2.48pre2.scm")
+
+(load "2.49a.scm")
+(load "2.49b.scm")
+(load "2.49c.scm")
+(load "2.49d.scm")
+
+(load "2.50pre.scm")
+(load "2.50.scm")
+
+(load "2.51a.scm")
+(load "2.51b.scm")
+
+(load "2.47a.scm")
+(load "2.47b.scm")
+(load "2.47c.scm")
+
+(load "2.44pre.scm")
+(load "2.44.scm")
+
+(load "2.45pre.scm")
+(load "2.45.scm")
+
+(load "2.52a.scm")
+(load "2.52b.scm")
+(load "2.52c.scm")
+
+; For testing
+(define o1 (make-vect 0.0 0.0))
+(define e1 (make-vect 1.0 0.0))
+(define e2 (make-vect 0.20 1.0))
+(define f1 (make-frame o1 e1 e2))
