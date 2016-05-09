d557e322a83120a043c543fee43d291aeb57cd41
Fabienne Ducroquet <fabiduc@gmail.com>
2016-05-09 21:18:40 +0200

    Exercise 2.52

---
+++ ch2/2.52c.scm
@@ -0,0 +1,4 @@
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split (flip-horiz painter) n))))
