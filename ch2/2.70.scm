9e54ca095d7dd7145b43ddbc4e7dc17531d2da3d
Fabienne Ducroquet <fabiduc@gmail.com>
2017-06-10 21:21:40 +0200

    Exercise 2.70

---
+++ ch2/2.70.scm
@@ -0,0 +1,5 @@
+(define rock-tree (generate-huffman-tree '((a 2) (boom 1) (get 2) (job 2)
+                                                 (na 16) (sha 3) (yip 9) (wah 1))))
+
+(define rock-message '(get a job sha na na na na na na na na get a job sha na na na na na na na na wah yip yip yip yip yip yip yip yip yip sha boom))
+
