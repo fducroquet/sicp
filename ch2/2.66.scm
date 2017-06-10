0e3385f46080eb775af590dfebb4818f8f2ad8b1
Fabienne Ducroquet <fabiduc@gmail.com>
2017-06-10 20:14:58 +0200

    Exercise 2.66

---
+++ ch2/2.66.scm
@@ -0,0 +1,8 @@
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (entry set-of-records)))
+         (entry set-of-records))
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-tree set-of-records)))
+        (else
+          (lookup given-key (right-tree set-of-records)))))
