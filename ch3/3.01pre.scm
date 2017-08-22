bfc75cae3ddd0d4bdbe7a84191a46ef8d1c7ff38
Fabienne Ducroquet <fabiduc@gmail.com>
2017-08-22 20:09:31 +0200

    Code from section 3.1.1

---
+++ ch3/3.01pre.scm
@@ -0,0 +1,47 @@
+(define balance 100)
+
+(define (withdraw amount)
+  (if (>= balance amount)
+    (begin (set! balance (- balance amount))
+           balance)
+    "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+        (begin (set! balance
+                 (- balance amount))
+               balance)
+        "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+      (begin (set! balance
+               (- balance amount))
+             balance)
+      "Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+(define W2 (make-withdraw 100))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance
+               (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+(define acc (make-account 100))
+(define acc2 (make-account 100))
