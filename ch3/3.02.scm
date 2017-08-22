503c7261a763b5e9c20ed397556bf4df26013336
Fabienne Ducroquet <fabiduc@gmail.com>
2017-08-22 20:14:09 +0200

    Exercise 3.2

---
+++ ch3/3.02.scm
@@ -0,0 +1,11 @@
+(define (make-monitored f)
+  (let ((count 0))
+    (define (mf x)
+      (cond ((eq? x 'how-many-calls?)
+             count)
+            ((eq? x 'reset-count)
+             (set! count 0))
+            (else
+              (set! count (+ count 1))
+              (f x))))
+    mf))
