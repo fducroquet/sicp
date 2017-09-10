512a255e4ac1f23670044da8124b1a2b72640bcf
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-10 23:16:51 +0200

    Rest of code from section 3.3.4

---
+++ ch3/3.31simul.scm
@@ -0,0 +1,15 @@
+; Sample simulation
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+
+(set-signal! input-2 1)
+(propagate)
