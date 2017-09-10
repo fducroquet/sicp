ec70409f4d665d3d1a964ca9d363979e92ddfc0c
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-10 23:18:51 +0200

    File for testing simulation

---
+++ ch3/digital-circuits.test.scm
@@ -0,0 +1,16 @@
+; Queues
+(load "3.21pre.scm")
+(load "3.21.scm")
+
+; Inverter, and-gate, half-adder, full-adder.
+(load "3.28pre.scm")
+; Or-gate
+(load "3.28.scm")
+; Or-gate, second version
+; (load "3.29.scm")
+; Ripple-carry adder
+(load "3.30.scm")
+; Wires and agenda
+(load "3.31pre.scm")
+; Simple simulation
+(load "3.31simul.scm")
