ab59b8d7542c856f4b21902fc6116e39ba56cfb4
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-10 16:06:59 +0200

    Code from beginning of section 3.3.4

---
+++ ch3/3.28pre.scm
@@ -0,0 +1,49 @@
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+; Added for the previous definition
+(define (logical-and s1 s2)
+  (cond ((or (not (valid-signal? s1))
+             (not (valid-signal? s2)))
+         (error "Invalid signal" (list s1 s2)))
+        ((and (= 1 s1) (= 1 s2)) 1)
+        (else 0)))
