f16bf6f776a96dc986c7c2c9c51d60b42f07aec7
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-13 21:32:40 +0200

    Exercise 3.37

---
+++ ch3/3.37pre.scm
@@ -0,0 +1,12 @@
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
