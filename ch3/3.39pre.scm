3ecd1bcb13a3c15926e1e65e69442a8efef16899
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-17 11:37:19 +0200

    Code from section 3.4.2

---
+++ ch3/3.39pre.scm
@@ -0,0 +1,127 @@
+; From code taken from the MIT’s SICP website
+(define disallow-preempt-current-thread
+  (access disallow-preempt-current-thread
+	  (->environment '(runtime thread))))
+
+(define allow-preempt-current-thread
+  (access allow-preempt-current-thread
+	  (->environment '(runtime thread))))
+
+(define (kill-thread thread)
+  (let ((event
+	 (lambda ()
+	   (exit-current-thread 'RIP))))
+    (without-interrupts
+     (lambda ()
+       (case (thread-execution-state thread)
+	 ((STOPPED) (restart-thread thread #t event))
+	 ((DEAD) unspecific)
+	 (else (signal-thread-event thread event)))))))
+
+(define (parallel-execute . thunks)
+  (let ((my-threads '()))
+    (define (terminator)
+      (without-interrupts
+       (lambda ()
+	 (for-each kill-thread my-threads)
+	 (set! my-threads '())
+	 unspecific)))
+    (without-interrupts
+     (lambda ()
+       (set! my-threads
+	     (map (lambda (thunk)
+		    (let ((thread (create-thread #f thunk)))
+		      (detach-thread thread)
+		      thread))
+		  thunks))
+       unspecific))
+    terminator))
+; End of part taken from SICP’s website
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+               (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (without-interrupts
+    (lambda ()
+      (if (car cell)
+        true
+        (begin (set-car! cell true)
+               false)))))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds."))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+    dispatch))
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds."))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+    dispatch))
+
+(define (deposit account amount)
+  (let ((s (account 'serializer))
+        (d (account 'deposit)))
+    ((s d) amount)))
+
+(define (withdraw account amount)
+  (let ((s (account 'serializer))
+        (w (account 'withdraw)))
+    ((s w) amount)))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
