9df67acea0915862bd9bed0da8ac48a323c1cd2b
Fabienne Ducroquet <fabiduc@gmail.com>
2017-09-17 17:45:41 +0200

    Exercise 3.52

---
+++ ch3/3.52.scm
@@ -0,0 +1,9 @@
+(define sum 0)
+
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
