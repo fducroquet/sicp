b814ad07777aebc2294e6753db2a4abefbe54026
Fabienne Ducroquet <fabiduc@gmail.com>
2018-02-11 21:16:14 +0100

    simulator-test: Put machine definitions at the end
    Otherwise, the machines defined after the file is loaded do not use the
    features defined in the exercises.

--- ch5/simulator-test.scm
+++ ch5/simulator-test.scm
@@ -1,18 +1,6 @@
 (load "5.07pre-simulator.scm")
-(load "5.01pre.scm") ; Gcd-machine (iterative).
-(load "5.02.scm") ; Fact-machine (iterative).
-; gcd-machine2: reads inputs and prints results.
-; gcd-machine3: does not use remainder as a primitive operation.
-(load "5.03pre.scm")
-(load "5.03asup.scm") ; Definitions used in 5.03a.
-(load "5.03a.scm") ; Square roots usign Newton’s method, complex primitives.
-(load "5.03b.scm") ; Square roots usign Newton’s method, expanded operations.
-(load "5.04pre.scm") ; Recursive fact-machine, Fibonacci-machine.
-(load "5.04a.scm") ; Recursive exponentiation.
-(load "5.04b.scm") ; Iterative exponentiation.
 (load "5.08.scm") ; Forbid using the same label name at several places.
 (load "5.09.scm") ; Forbid labels in operation arguments.
-(load "5.11a.scm") ; Fibonacci-machine with one fewer instruction.
 
 ; Store additional information in the machine model. 
 (load "5.12set.scm")
@@ -31,3 +19,16 @@
 
 ; Enable registers to be traced.
 (load "5.18.scm")
+
+(load "5.01pre.scm") ; Gcd-machine (iterative).
+(load "5.02.scm") ; Fact-machine (iterative).
+; gcd-machine2: reads inputs and prints results.
+; gcd-machine3: does not use remainder as a primitive operation.
+(load "5.03pre.scm")
+(load "5.03asup.scm") ; Definitions used in 5.03a.
+(load "5.03a.scm") ; Square roots usign Newton’s method, complex primitives.
+(load "5.03b.scm") ; Square roots usign Newton’s method, expanded operations.
+(load "5.04pre.scm") ; Recursive fact-machine, Fibonacci-machine.
+(load "5.04a.scm") ; Recursive exponentiation.
+(load "5.04b.scm") ; Iterative exponentiation.
+(load "5.11a.scm") ; Fibonacci-machine with one fewer instruction.
