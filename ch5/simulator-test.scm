12822d767ccd7b4d3eee8983a560a6d4f3fa4299
Fabienne Ducroquet <fabiduc@gmail.com>
2018-03-23 20:39:34 +0100

    register simulator: Fix included file name for exercise 5.19

--- ch5/simulator-test.scm
+++ ch5/simulator-test.scm
@@ -23,7 +23,7 @@
 ; Breakpoints.
 (load "5.19a.scm")
 (load "5.19b.scm")
-(load "5.19c.scm")
+(load "5.19d.scm")
 
 (load "5.01pre.scm") ; Gcd-machine (iterative).
 (load "5.02.scm") ; Fact-machine (iterative).
